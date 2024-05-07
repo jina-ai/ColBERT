bde4bcf5d551c42105190a964ff0996c5351bf13
liuqi6777 <qiliu6777@gmail.com>
2024-05-07 11:07:32 +0200

    fix: load beir

--- colbert/evaluation/loaders.py
+++ colbert/evaluation/loaders.py
@@ -20,7 +20,10 @@ def load_queries(queries_path):
     with open(queries_path) as f:
         for line in f:
             qid, query, *_ = line.strip().split('\t')
-            qid = int(qid)
+            try:
+                qid = int(qid)
+            except:
+                pass
 
             assert (qid not in queries), ("Query QID", qid, "is repeated!")
             queries[qid] = query
@@ -163,7 +166,7 @@ def load_collection(collection_path):
                 print(f'{line_idx // 1000 // 1000}M', end=' ', flush=True)
 
             pid, passage, *rest = line.strip('\n\r ').split('\t')
-            assert pid == 'id' or int(pid) == line_idx, f"pid={pid}, line_idx={line_idx}"
+            # assert pid == 'id' or int(pid) == line_idx, f"pid={pid}, line_idx={line_idx}"
 
             if len(rest) >= 1:
                 title = rest[0]
